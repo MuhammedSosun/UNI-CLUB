55c0c76bb31d69d2ec049c48a2fc25626e4d9580
Muhammed <muhammedsosun06@gmail.com>
2025-10-03 22:02:29 +0300

    token
    Token yetkilendirmeleri için objeler ve servislerdeki methodlar yazıldı

--- src/main/java/com/uniClub/user/internal/service/IAuthenticateService.java
+++ src/main/java/com/uniClub/user/internal/service/IAuthenticateService.java
@@ -1,4 +1,9 @@
 package com.uniClub.user.internal.service;
 
+import com.uniClub.user.api.dto.*;
+
 public interface IAuthenticateService {
+    UserDto register(AuthRequest authRequest);
+    AuthResponse authenticate(AuthRequest authRequest);
+    AuthResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
 }
--- src/main/java/com/uniClub/user/internal/service/impl/AuthenticateServiceImpl.java
+++ src/main/java/com/uniClub/user/internal/service/impl/AuthenticateServiceImpl.java
@@ -1,4 +1,39 @@
 package com.uniClub.user.internal.service.impl;
 
-public class AuthenticateServiceImpl {
+import com.uniClub.user.api.dto.*;
+import com.uniClub.user.internal.entity.RefreshToken;
+import com.uniClub.user.internal.entity.UserEntity;
+import com.uniClub.user.internal.service.IAuthenticateService;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class AuthenticateServiceImpl implements IAuthenticateService {
+
+
+
+    private RefreshToken createRefreshToken(UserEntity user) {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUser(user);
+        refreshToken.setExpiredDate(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *4));
+        refreshToken.setRefreshToken(UUID.randomUUID().toString());
+
+        return refreshToken;
+    }
+    @Override
+    public UserDto register(AuthRequest authRequest) {
+        return null;
+    }
+
+    @Override
+    public AuthResponse authenticate(AuthRequest authRequest) {
+        return null;
+    }
+
+    @Override
+    public AuthResponse refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        return null;
+    }
 }
